3d3a5fda321083a2e4b9cb880db47b05f0a96adc
matt <bronx0807@gmail.com>
2020-09-06 00:43:57 +0800

    format

--- src/main/java/com/abc/demo/DemoServlet.java
+++ src/main/java/com/abc/demo/DemoServlet.java
@@ -15,41 +15,38 @@ import com.abc.demo.dto.EmployeeDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class DemoServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     public DemoServlet() {
         super();
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    String pathInfo = request.getPathInfo();
-	    
-	    if (StringUtils.isEmpty(pathInfo)) {
-	        response.sendError(HttpServletResponse.SC_NOT_FOUND);
-	        return;
-	    }
-	    
-	    String path = pathInfo.split("/")[1];
-	    
-	    ObjectMapper objectMapper = new ObjectMapper();
-	    String jsonString = "";
-	    PrintWriter writer = response.getWriter();
-	    
-	    if ("employee".equalsIgnoreCase(path)) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String pathInfo = request.getPathInfo();
+
+        if (StringUtils.isEmpty(pathInfo)) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        String path = pathInfo.split("/")[1]; // e.g.: /employee
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = "";
+        PrintWriter writer = response.getWriter();
+
+        if ("employee".equalsIgnoreCase(path)) {
             EmployeeDto employeeDto = new EmployeeDto(1L, "john", "john@abc.com", 28);
             ApiResposne apiResposne = new ApiResposne("success", employeeDto);
             jsonString = objectMapper.writeValueAsString(apiResposne);
             writer.write(jsonString);
             return;
         }
-	    
-	    response.sendError(HttpServletResponse.SC_NOT_FOUND);
-	    writer.write(jsonString);
-	    return;
-    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+        response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        writer.write(jsonString);
+        return;
+    }
 
 }
--- src/main/java/com/abc/demo/dto/ApiResposne.java
+++ src/main/java/com/abc/demo/dto/ApiResposne.java
@@ -1,25 +1,26 @@
 package com.abc.demo.dto;
 
 public class ApiResposne {
-    
+
     private String message;
     private Object data;
-    
+
     public ApiResposne() {
     }
-    
+
     public ApiResposne(String message) {
         this.message = message;
     }
-    
+
     public ApiResposne(String message, Object data) {
         this.message = message;
         this.data = data;
     }
-    
+
     public String getMessage() {
         return message;
     }
+
     public void setMessage(String message) {
         this.message = message;
     }
@@ -27,6 +28,7 @@ public class ApiResposne {
     public Object getData() {
         return data;
     }
+
     public void setData(Object data) {
         this.data = data;
     }
