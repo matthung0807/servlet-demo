12a3e31f11690647a0b154a42f379b7351d49eea
matthung0807 <bronx0807@gmail.com>
2020-09-19 16:07:24 +0800

    servlet url mapping

--- src/main/java/com/abc/demo/DemoServlet.java
+++ src/main/java/com/abc/demo/DemoServlet.java
@@ -8,17 +8,15 @@ import javax.servlet.http.HttpServletResponse;
 
 public class DemoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public DemoServlet() {
-        super();
-    }
+    
+    private int count = 0; // Request共用Servlet的成員變數，所以Servlet不應該設定成員變數。
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		count++;
+		
+		response.getWriter().append("count = " + count);
 	}
 
 }
